3b2b736bd74241c185ac8a091130e87e25248694
BrandonRPS <brandon.sidebottom@rpssoftware.co.uk>
2021-11-08 00:29:47 +0000

    find the cell of the apple

--- tests/findapplecell.py
+++ tests/findapplecell.py
@@ -42,7 +42,7 @@ def get_board_end(img):
     col = [170, 215, 81, 255]
     for h in reversed(range(0, len(img))):
         for w in reversed(range(0, 600)):
-            if (compare(img[h, w], col)):
+            if compare(img[h, w], col):
                 return [w,h]
 
 def divide_game_board(start, end, divisions = 17):
@@ -63,10 +63,64 @@ def create_cell_list(s, w, h, dx, dy):
             tly = s[1] + (cellh * divY)
             brx = tlx + cellw
             bry = tly + cellh
-            cell = [[tlx, tly], [brx, bry]]
+            cell = [[tlx, tly], [brx, bry], [cellw, cellh]]
             cells.append(cell)
     
     return cells
+
+def get_target_rect(img, templatepath):
+    img2 = img.copy()
+    template = cv.imread(templatepath, 1)
+    template = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
+    w, h = template.shape[::-1]
+
+    method=cv.TM_CCOEFF_NORMED
+    gray = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+    res = cv.matchTemplate(gray, template, method)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+        top_left = min_loc
+    else:
+        top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+
+    return top_left, bottom_right
+
+def get_apple_cell(start, end, cells, t = 10):
+    
+    ac = get_center_point(start, end)
+
+    #compare = lambda x, y: (collections.Counter(x) - collections.Counter(y)  t)
+    for cell in cells:
+        tl = [int(cell[0][0]), int(cell[0][1])]
+        br = [int(cell[1][0]), int(cell[1][1])]
+        cc = get_center_point(tl, br)
+
+        #print (ac)
+        #print (cc)
+        #print(end)
+        xDif = cc[0] - ac[0]
+        print (f"XDIF = {xDif}")
+        yDif = cc[1] - ac[1]
+        print (f"YDIF = {yDif}")
+
+        if (xDif < t and xDif > (0 - t)):
+            print(f"XDIF of {xDif} is lower than threshold{t}")
+            if (yDif < t and yDif > (0 - t)):
+                print (f"YDIF of {yDif} is lower than threshold{t}")
+                return cell
+
+        #if compare (cc, ac):
+        #print(f'MATCHING CELL = {cell}')
+            #return cell
+
+def get_center_point(start, end):
+    cx = (start[0] + end[0]) / 2
+    cy = (start[1] + end[1]) / 2
+    print (f"Center of: {start[0]}, {end[0]} = {cx}")
+    return [int(cx), int(cy)]
+
 
 def main():
     game = Screen_Shot()
@@ -78,6 +132,13 @@ def main():
         tl = [int(cell[0][0]), int(cell[0][1])]
         br = [int(cell[1][0]), int(cell[1][1])]
         draw_rectangle(game, tl, br, (0, 255, 0))
+    
+    apple = get_target_rect(game, 'img/apple.jpg')
+    draw_rectangle(game, apple[0], apple[1], (255, 0, 0))
+
+    ac = get_apple_cell(apple[0], apple[1], game_cells, 10)
+    draw_rectangle(game, [int(ac[0][0]), int(ac[0][1])],  [int(ac[1][0]), int(ac[1][1])], (0, 0, 255))
+
     running = True
     while running:
         cv.imshow('res', game)
@@ -87,6 +148,5 @@ def main():
 		        cv.waitKey(1)
 		        flag = False
 		        sys.exit
-
 
 main()
